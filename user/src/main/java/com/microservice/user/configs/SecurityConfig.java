b7f6de9c87bc0687b95aa954567b0d1fae126e86
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-01-23 23:45:46 -0300

    [Microservices user FIX] Liberando a rota error/ para funcionar as minhas exceções

--- user/src/main/java/com/microservice/user/configs/SecurityConfig.java
+++ user/src/main/java/com/microservice/user/configs/SecurityConfig.java
@@ -39,6 +39,8 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/error").permitAll() 
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
