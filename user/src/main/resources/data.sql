fba65624d62a98ccd075a242845badc9caf0f34e
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-01-24 17:12:33 -0300

    [Microservices user ADD] Dockerfile e variáveis de ambiente

--- user/src/main/resources/data.sql
+++ user/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT IGNORE INTO TB_ROLES (role_id, name) VALUES (1, 'ADMIN');
-INSERT IGNORE INTO TB_ROLES (role_id, name) VALUES (2, 'BASIC');
+INSERT IGNORE INTO tb_roles (role_id, name) VALUES (1, 'ADMIN');
+INSERT IGNORE INTO tb_roles (role_id, name) VALUES (2, 'BASIC');
